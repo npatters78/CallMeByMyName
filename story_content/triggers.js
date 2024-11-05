b3c6b72e52fc705b413d6023c6763613439b4b4e
npatters78 <npatter2@gmail.com>
2024-11-05 18:08:45 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5WYIySpDRTu":
+        Script1();
+        break;
+      case "5iosgfLOgIN":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
